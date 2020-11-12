88973ce9e24fb0462f718520604580676b3277a7
Mark Tartagashev <mangoozt@gmail.com>
2020-11-13 00:39:24 +0300

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -2,10 +2,11 @@
 for d in */*/ ; do
 	cd $d
 	mv constraints*.json constraints.json
-	mv hmi-data*.json hmi-data.json
-	mv nav-data*.json nav-data.json
-	mv route-data*.json route-data.json
+	mv hydrometeo*.json hmi-data.json
+	mv navigation*.json nav-data.json
+	mv route.json route-data.json
 	mv settings*.json settings.json
-	mv target-data*.json target-data.json
+	mv targets.json target-data.json
+	mv targets_settings*.json target-settings.json
 	cd ../..
 done
